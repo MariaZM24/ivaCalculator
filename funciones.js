9e3b500d5860ff15ad45207b2aa385859399c680
Maria ZM <34519730+MariaZM24@users.noreply.github.com>
2020-12-14 16:01:13 +0100

    ejercicio dado 10 caras + calculador iva

---
+++ funciones.js
@@ -0,0 +1,17 @@
+Dado 10 caras 
+function tirarDados(min, max) {
+var dados = Math.round(Math.random() * (max - min)) + 1;
+return dados;
+}
+tirarDados(1, 10);
+
+
+
+Calculadora IVA
+function IVACalculator (money) {
+    var IVACalculator = money * IVA; 
+    return alert("Tienes que pagar " + IVACalculator + " euros de IVA.")
+}
+
+var IVA = 0.21;
+IVACalculator (2300)
